b6029cba9dcad0ea8eef34dd80c3505322c8f3a2
void-debug-coder <mwamwachaezekia@gmail.com>
2026-05-10 12:18:55 +0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'voidhub-v1';
+const urlsToCache = [
+  '/',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
